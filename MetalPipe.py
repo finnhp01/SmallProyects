48180ed9905799c23c34094a667af5662e7dadda
finnhp01 <finnhp01@gmail.com>
2026-08-05 23:53:12 +0200

    ...

---
+++ MetalPipe.py
@@ -0,0 +1,24 @@
+from pynput import mouse, keyboard
+import pygame
+from pathlib import Path
+
+pygame.mixer.init()
+pipe_path = Path(__file__).parent / "ThePipe.ogg"
+pipe = pygame.mixer.Sound("ThePipe.ogg")
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        print("Mouse click!")
+        pipe.play()
+    else:
+        print("PIPE!")
+
+def on_press(key):
+    global listener
+    if key == keyboard.Key.esc:
+        listener.stop()
+        return False
+
+with mouse.Listener(on_click=on_click) as listener:
+    with keyboard.Listener(on_press=on_press) as klistener:
+        listener.join()
